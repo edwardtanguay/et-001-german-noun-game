4b1b4fcaee62cd1ca8e583621d1a4d322168090a
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-03 01:04:44 +0100

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import { useState, useEffect } from 'react';
 import './App.scss';
+import axios from 'axios';
+
+const url = 'https://edwardtanguay.vercel.app/share/germanNouns.json';
+
+interface INoun {
+	article: string;
+	singular: string;
+	plural: string;
+}
 
 function App() {
+	const [nouns, setNouns] = useState<INoun[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			setNouns((await axios.get(url)).data);
+		})();
+	}, []);
+
 	return (
 		<div className="App">
 			<h1>German Noun Game</h1>
-			<p>Welcome to this site.</p>
+			<h2>There are {nouns.length} nouns:</h2>
+			<div className="nouns">
+				{nouns.map((noun) => {
+					return (
+						<div className="noun" key={noun.singular}>
+							<div className="singular">
+								{noun.article} {noun.singular}
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
